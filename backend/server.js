3e75d1ccb045289fdc66f2b217898b506b8e2cdd
Aryans27 <sakshitaryan@gmail.com>
2022-07-10 11:16:31 +0530

    2nd commit (Chat Page)

--- backend/server.js
+++ backend/server.js
@@ -6,19 +6,38 @@ const socketIO = require('socket.io');
 const app = express();        // importing all the functions of express into const app
 const port = 4500 || process.env.PORT;
 
+const users = [{}]        // array of obj
 
 app.use(cors());        // used for inter communication b/w URL
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello it's working!!!")
 })
 
 const server = http.createServer(app);    // creating server and calling express(using app variable)
 
-const io = socketIO(server);  // establishing the connection with the server
+const io = socketIO(server);  // establishing the connection with the server. io is the entire ckt
 
-io.on("connection", ()=>{       // when the ckt gets on 
+io.on("connection", (socket) => {       // when the ckt gets on 
     console.log('New Connection');
+
+    socket.on('joined', ({ user }) => {        // sockets are individual users, destructuring data.user 
+        users[socket.id] = user;       // every socket has a unique id
+        // console.log(`${user} has joined`);
+        socket.broadcast.emit('userJoined', { user: "Admin", message: `${users[socket.id]} has joined` })      // broadcast means, the the msg will go to all members except the  joiny
+        socket.emit('welcome', { user: "Admin", message: `${users[socket.id]} Welcome to the chat` })
+    })
+
+    socket.on('message', ({ message, id }) => {
+        io.emit('sendMessage', { user: users[id], message, id })     // to circulate the message into the entire circuit, thus we use io, not socket, users[id] will give the name of the user
+    })
+
+    socket.on('disconnected', () => {        // when the user leaves the chats
+        socket.broadcast.emit('leave', { user: "Admin", message: `${users[socket.id]} has left the chats` });
+        console.log(`${users[socket.id]} left`);
+    });
 })
+
+
 server.listen(port, () => {
     console.log(`server is working on http://localhost: ${port}`);
 })
--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,13 +1,74 @@
-import React from 'react'
-import {user} from "../Join/Join"
+import React, { useEffect, useState } from 'react'
+import { user } from "../Join/Join"
+import socketIOClient from 'socket.io-client'
+import sendLogo from '../../images/send.png'
+import './Chat.css'
 
+const ENDPOINT = "http://localhost:4500/";     // ENPOINT is the pt from where the socket is to be received
+let socket;
 
-function Chat() {
-  return (
-    <div>
-        <h1>{user}</h1>
-    </div>
-  )
+const Chat = () => {
+
+    const [id, setId] = useState("");
+    const [message, setMessage] = useState([])
+
+    const send = () => {
+        const message = document.getElementById('chatInput').value;
+        socket.emit('message', { message, id });
+        document.getElementById('chatInput').value = "";
+    }
+
+    useEffect(() => {
+        socket = socketIOClient(ENDPOINT, { transports: ['websocket'] });
+
+        socket.on('connect', () => {        // on means receiving data
+            alert('Connected');
+            setId(socket.id);
+        })
+
+        socket.emit('joined', { user })        // emit means the data is being send
+
+        socket.on('welcome', (data) => {
+            console.log(data.user, data.message);
+        })
+
+        socket.on('userJoined', (data) => {
+            console.log(data.user, data.message);
+        })
+
+        socket.on('leave', (data) => {
+            console.log(data.user, data.message);
+
+        })
+
+        return () => {
+            socket.emit('disconnected');
+            socket.off();
+        }
+    }, [])
+
+    useEffect(() => {
+      socket.on('sendMessage', (data)=>{
+        console.log(data.user, data.message, data.id) 
+      })
+    
+      return () => {
+        
+      }
+    }, [])
+    
+    return (
+        <div className="chatPage">
+            <div className="chatContainer">
+                <div className="header"><h2>Flingrr</h2></div>
+                <div className="chatBox"></div>
+                <div className="inputBox">
+                    <input type="text" id="chatInput" />
+                    <button onClick={send} className="sendBtn"><img src={sendLogo} alt="Send" /></button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Chat
